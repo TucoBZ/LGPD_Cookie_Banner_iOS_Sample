84fe087cdefa4c0d5aa46d6935af70b76b57a020
Tulio Bazan <tulio.bazan@dextra-sw.com>
2020-09-04 09:33:23 -0300

    fix: SceneDelegate

--- LGPDBannerSample/Resource/SceneDelegate.swift
+++ LGPDBannerSample/Resource/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
-        let firstController = UIViewController()
+        let firstController = ViewController()
         firstController.title = "Home"
         firstController.tabBarItem.title = "Home"
         firstController.tabBarItem.image = UIImage(systemName: "house")
